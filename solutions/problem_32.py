09dea0c8dd1e68248d048ded93af03773eda0651
Jimmy Börjesson <alcesleo@gmail.com>
2020-02-25 17:31:33 +0100

    Solve problem 32

---
+++ solutions/problem_32.py
@@ -0,0 +1,48 @@
+from common.tools import digits
+
+DIGITS = set(range(1, 10))
+
+
+def unique_digits(n):
+    """Returns the unique digits of n as a set, or None if any digits are repeated
+    """
+    d = digits(n)
+    s = set(d)
+
+    if len(d) == len(s):
+        return s
+
+
+limit = 2000
+products = set()
+
+for a in range(1, limit):
+    digits_a = unique_digits(a)
+    if not digits_a:
+        continue
+
+    for b in range(a + 1, limit):
+        digits_b = unique_digits(b)
+        if not digits_b:
+            continue
+
+        # a and b contain same digit
+        if not digits_a.isdisjoint(digits_b):
+            continue
+
+        p = a * b
+
+        digits_p = unique_digits(p)
+        if not digits_p:
+            continue
+
+        no_common_digits = digits_a.isdisjoint(digits_b) and digits_b.isdisjoint(
+            digits_p) and digits_p.isdisjoint(digits_a)
+
+        contains_all_digits = (digits_a | digits_b | digits_p) == DIGITS
+
+        if no_common_digits and contains_all_digits:
+            products.add(p)
+
+
+print(sum(products))
