276f4c7fcedb379d4f5084f050e2b1789aa910f9
Jimmy Börjesson <alcesleo@gmail.com>
2020-02-29 11:53:25 +0100

    Optimize problem 31
    This brings the runtime down from ~90s to ~3s by using a hashable data structure,
    frozenset, which can in turn be put into a set instead of a list making the
    detection of duplicate counts O(log n) instead of O(n).
    
    The collections.Counter class is conceptually the perfect data type to store
    the counts, but the performance characteristics of set(frozenset(tuple)) is
    way better and only slightly less ergonomic.

--- solutions/problem_031.py
+++ solutions/problem_031.py
@@ -1,40 +1,29 @@
-from collections import Counter
-
-
 def combinations(coins, target):
     """Returns the combinations of coin counts that equal a target sum
     """
-    if len(coins) == 0:
-        return []
+
+    # Store the counts as a set(frozenset(tuple(coin, count)))
+    #
+    # The frozenset of tuples is hashable unlike the collections.Counter
+    # class which would be a conceptually nicer data structure, but isn't hashable
+    result = set()
+
+    if len(coins) == 0:  # TODO: Move down
+        return result
 
     coin, *rest = coins
     most = target // coin
 
-    result = []
-
     for count in range(0, most + 1):
         total = coin * count
         remainder = target - total
-        combination = Counter([coin] * count)
+        combination = frozenset([(coin, count)])
 
-        if total == target:
-            result.append(combination)
+        if total == target and count != 0:
+            result.add(combination)
 
         for rest_combination in combinations(rest, remainder):
-            rest_total = combination + rest_combination
-            result.append(combination + rest_combination)
-
-    return result
-
-
-def deduplicate(c):
-    """Deduplicates an unhashable collection
-    """
-    result = []
-
-    for i in c:
-        if i not in result:
-            result.append(i)
+            result.add(combination | rest_combination)
 
     return result
 
@@ -42,6 +31,6 @@ def deduplicate(c):
 TARGET = 200
 COINS = [200, 100, 50, 20, 10, 5, 2, 1]
 
-result = deduplicate(combinations(COINS, TARGET))
+result = combinations(COINS, TARGET)
 
 print(len(result))
