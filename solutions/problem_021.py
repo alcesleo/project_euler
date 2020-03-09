ee5e535d7b0320026a949296e09037b7b0e8e8f5
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-09 14:23:43 +0100

    Optimize problem 21

--- solutions/problem_021.py
+++ solutions/problem_021.py
@@ -1,11 +1,16 @@
+from functools import lru_cache
 from common.tools import proper_divisors
+
+LIMIT = 10_000
+
+@lru_cache(maxsize=LIMIT)
+def sum_proper_divisors(n):
+    return sum(proper_divisors(n))
 
 
 def is_amicable(a, b):
-    return a != b and sum(proper_divisors(a)) == b and sum(proper_divisors(b)) == a
+    return a != b and sum_proper_divisors(a) == b and sum_proper_divisors(b) == a
 
-
-LIMIT = 10_000
 
 result = 0
 for a in range(1, LIMIT):
