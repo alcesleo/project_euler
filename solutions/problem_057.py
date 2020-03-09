b5246d0e95b23538c4d96762c25855380803817f
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-09 14:56:06 +0100

    Solve problem 57

---
+++ solutions/problem_057.py
@@ -0,0 +1,56 @@
+from common.tools import split_digits
+from fractions import Fraction
+import sys
+
+
+LIMIT = 1000
+
+sys.setrecursionlimit(LIMIT * 2)
+
+
+def expand_root_two(iterations):
+    """Expands the square root of two by a number of iterations, equivalent to the following:
+
+    1 + Fraction(1, 2)
+    1 + Fraction(1, 2 + Fraction(1, 2))
+    1 + Fraction(1, 2 + Fraction(1, 2 + Fraction(1, 2)))
+    1 + Fraction(1, 2 + Fraction(1, 2 + Fraction(1, 2 + Fraction(1, 2))))
+
+    >>> expand_root_two(0)
+    Fraction(3, 2)
+
+    >>> expand_root_two(1)
+    Fraction(7, 5)
+
+    >>> expand_root_two(2)
+    Fraction(17, 12)
+
+    >>> expand_root_two(3)
+    Fraction(41, 29)
+    """
+    return 1 + Fraction(1, denominator(iterations))
+
+
+def denominator(iterations):
+    if iterations == 0:
+        return 2
+
+    return 2 + Fraction(1, denominator(iterations - 1))
+
+
+def count_digits(n):
+    return len(split_digits(n))
+
+
+def solve():
+    result = 0
+
+    for i in range(LIMIT):
+        expansion = expand_root_two(i)
+
+        if count_digits(expansion.numerator) > count_digits(expansion.denominator):
+            result += 1
+
+    return result
+
+print(solve())
