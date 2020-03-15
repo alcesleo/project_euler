4fd246af7dfbd6e0cef5e35f060e49beb5846dbc
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-15 18:51:00 +0100

    Add module docstring to problem 96

--- solutions/problem_097.py
+++ solutions/problem_097.py
@@ -1,1 +1,13 @@
-print((28433 * 2 ** 7830457 + 1) % 10 ** 10)
+"""
+Python has no issue working with numbers of this size, the only clever bit is using the modulo
+operator to extract the last 10 digits, as turning it into a string **does** take a long time.
+"""
+
+
+def solve():
+    return (28433 * 2 ** 7830457 + 1) % 10 ** 10
+
+
+if __name__ == "__main__":
+    result = solve()
+    print(result)
