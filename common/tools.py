44b6172c8901ba566acdb97f8d3387a3f1e9dbb5
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-16 17:04:45 +0100

    Refactor factors() function

--- common/tools.py
+++ common/tools.py
@@ -9,9 +9,19 @@ def factors(n):
     Returns all the factors of n as a set
 
     https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
+
+    >>> factors(15)
+    {1, 3, 5, 15}
     """
-    return set(reduce(list.__add__,
-                      ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+    result = set()
+
+    # Each factor below sqrt(n) will have a matching factor above sqrt(n) - n / i
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            result.add(i)
+            result.add(n // i)
+
+    return result
 
 
 @lru_cache(maxsize=256)
