60f6ac2c2dd8803c32d0717f797c2fc30c7afc9b
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-16 20:19:21 +0100

    Solve problem 72

---
+++ solutions/problem_072.py
@@ -0,0 +1,30 @@
+"""
+The amount of fractions for each n is the Euler's Totient
+"""
+
+from common.tools import phi
+
+
+def solve(limit=8):
+    result = 0
+    for n in range(2, limit + 1):
+        result += phi(n)
+
+    return result
+
+
+def verify(limit=8):
+    """Verify that the solve() method gives the same result as this more obviously correct but slow method.
+    """
+    result = set()
+
+    for d in range(2, limit + 1):
+        for n in range(1, d):
+            result.add(Fraction(n, d))
+
+    return len(result)
+
+
+if __name__ == "__main__":
+    result = solve(1_000_000)
+    print(result)
