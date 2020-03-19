3eb16af6cbb7d39a60dc151bd0225e2de0e2d3bf
Jimmy Börjesson <alcesleo@gmail.com>
2020-03-19 13:40:54 +0100

    Optimize problem 31
    This was not super slow, though it was taking around a second to run.
    By not keeping track of all the partitions, and only counting them, it
    gets a little more efficient, but as usual - converting the arguments
    to hashable types and slapping on @lru_cache makes it blazingly fast.

--- solutions/problem_031.py
+++ solutions/problem_031.py
@@ -1,36 +1,39 @@
-def combinations(coins, target):
-    """Returns the combinations of coin counts that equal a target sum
+from functools import lru_cache
+
+
+@lru_cache(maxsize=2048)
+def partitions(n, numbers=None):
+    """Returns the amount of partitions of n into a set of numbers
     """
+    if numbers == None:
+        numbers = tuple(reversed(range(1, n + 1)))
+        return partitions(n, numbers)
 
-    # Store the counts as a set(frozenset(tuple(coin, count)))
-    #
-    # The frozenset of tuples is hashable unlike the collections.Counter
-    # class which would be a conceptually nicer data structure, but isn't hashable
-    result = set()
+    if len(numbers) == 0:
+        return 0
 
-    if len(coins) == 0:  # TODO: Move down
-        return result
-
-    coin, *rest = coins
-    most = target // coin
+    number, *rest = numbers
+    most = n // number
+    result = 0
 
     for count in range(0, most + 1):
-        total = coin * count
-        remainder = target - total
-        combination = frozenset([(coin, count)])
+        total = number * count
+        remainder = n - total
 
-        if total == target and count != 0:
-            result.add(combination)
+        if total == n and count != 0:
+            result += 1
 
-        for rest_combination in combinations(rest, remainder):
-            result.add(combination | rest_combination)
+        result += partitions(remainder, tuple(rest))
 
     return result
 
 
-TARGET = 200
-COINS = [200, 100, 50, 20, 10, 5, 2, 1]
+def solve():
+    target = 200
+    coins = (200, 100, 50, 20, 10, 5, 2, 1)
 
-result = combinations(COINS, TARGET)
+    return partitions(target, coins)
 
-print(len(result))
+
+if __name__ == "__main__":
+    print(solve())
