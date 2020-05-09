1f3fff9bcc6aab8c9cf3e4e4730ee91368a2c20a
Jimmy Börjesson <alcesleo@gmail.com>
2020-05-09 17:00:41 +0200

    Refactor problem 14

--- solutions/problem_014.py
+++ solutions/problem_014.py
@@ -12,16 +12,17 @@ def gen_collatz(start):
         yield(n)
 
 
-LIMIT = 1_000_000
+def solve(limit=1_000_000):
+    longest_chain, starting_number = 0, 0
 
-longest_chain = 0
-starting_number = 0
+    for i in range(1, limit):
+        sequence_length = len(list(gen_collatz(i)))
 
-for i in range(1, LIMIT):
-    sequence_length = len(list(gen_collatz(i)))
-    if sequence_length > longest_chain:
-        longest_chain = sequence_length
-        starting_number = i
+        if sequence_length > longest_chain:
+            longest_chain, starting_number = sequence_length, i
+
+    return starting_number
 
 
-print(starting_number)
+if __name__ == "__main__":
+    print(solve())
