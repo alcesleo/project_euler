3b1fe946a3bbe05fb0ee774c4765541db3571127
Jimmy Börjesson <alcesleo@gmail.com>
2020-06-29 16:57:03 +0200

    Improve Sudoku parsing

--- solutions/problem_096.py
+++ solutions/problem_096.py
@@ -1,3 +1,4 @@
+import re
 from common.data import read_data
 from common.digits import split_digits, join_digits
 from common.tools import slice_grid
@@ -10,27 +11,19 @@ EMPTY = {0}
 
 
 def parse_sudokus(data):
-    """Returns a list of numpy 2d-arrays representing sudokus
-    """
+    """Returns a list of numpy 2d-arrays representing sudokus."""
     sudokus = []
-    current = []
+    sudoku_data = filter(None, re.split(r"Grid \d\d\n", data))
 
-    for row in data.splitlines():
-        if "Grid" in row:
-            continue
-
-        current.append(list(split_digits(row)))
-
-        if len(current) == SIZE:
-            sudokus.append(current)
-            current = []
+    for sudoku in sudoku_data:
+        parsed_sudoku = list(map(list, map(split_digits, sudoku.splitlines())))
+        sudokus.append(parsed_sudoku)
 
     return sudokus
 
 
 def region(sudoku, row, col):
-    """Returns the numbers in a 3x3 region of a sudoku that row and col is in
-    """
+    """Returns the numbers in a 3x3 region of a sudoku that row and col is in."""
     region_row = row // 3 * 3
     region_col = col // 3 * 3
 
@@ -40,8 +33,7 @@ def region(sudoku, row, col):
 
 
 def options(sudoku, row, col):
-    """Returns a set of numbers that are legal to put into a cell
-    """
+    """Returns a set of numbers that are legal to put into a cell."""
     filled_row = set(sudoku[row]) - EMPTY
     filled_col = set(slice_grid(sudoku, col=col, vstep=1)) - EMPTY
     filled_reg = set(region(sudoku, row, col)) - EMPTY
@@ -52,8 +44,7 @@ def options(sudoku, row, col):
 
 
 def solve_sudoku(sudoku, cell=0):
-    """Recursively solves a sudoku by trying all reasonable options depth first
-    """
+    """Recursively solves a sudoku by trying all reasonable options depth first."""
     if cell == SIZE * SIZE:
         return True
 
